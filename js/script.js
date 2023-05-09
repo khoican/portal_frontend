5e21919c9199eb2ad66c7a9ddf7fc36bb9dd6b0c
khoican <ibnukhoirulprasetyo@gmail.com>
2023-05-09 20:54:05 +0700

    load data form api

--- js/script.js
+++ js/script.js
@@ -1,8 +1,36 @@
-function getData() {
-    fetch("http://localhost:3000/in")
-        .then(res => { return res.json(); })
-        .then(data => { console.log(data.data); })
+import './app';
+
+const tableIn = document.querySelector('#table');
+const tbody = document.querySelector('#tbody');
+const url = "http://localhost:3000/in"
+
+async function getData() {
+    await fetch(url)
+        .then(res => { 
+            res.json()
+                .then(data => {
+                    const dataJson = data.data
+                    let no = 1
+                    let output = ``
+                    
+                    dataJson.map((result) => {
+                        let time = result.waktu;
+                        let newTime = time.replace("T", " - ").replace(".000Z", "");
+
+                        output += `
+                            <tr>
+                                <td>${ no++ }</td>
+                                <td>${ result.nama }</td>
+                                <td>${ result.kompi }</td>
+                                <td>${ newTime }</td>
+                            </tr>
+                        `
+                        tbody.innerHTML = output
+                    })
+                })
+        })
         .catch(error => console.log(error))
 }
 
 getData();
+
